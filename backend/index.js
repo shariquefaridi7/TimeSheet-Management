8d3372896337094454dae74f0531e0db12ce7b81
shariquefaridi7 <shariquefaridi786@gmail.com>
2024-02-15 23:07:21 +0530

    FINAL

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,12 @@ import mongoose from 'mongoose';
 import empInfoRoute from "./routes/EmpInfo.js";
 import sheetRoute from './routes/TimeSheet.js';
 
-const app=express();
+const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-const PORT=4000;
-app.use("/",empInfoRoute);
-app.use("/",sheetRoute);
+app.use(express.urlencoded({ extended: true }));
+const PORT = 4000;
+app.use("/", empInfoRoute);
+app.use("/", sheetRoute);
 
-mongoose.connect(`mongodb+srv://project1:jtQgREqnQpXqKB9a@cluster0.vjk5qmt.mongodb.net/?retryWrites=true&w=majority`).then(()=>{app.listen(PORT,()=>console.log("server start at "+PORT))}).catch(()=>console.log("error"));
+mongoose.connect(`mongodb+srv://project1:jtQgREqnQpXqKB9a@cluster0.vjk5qmt.mongodb.net/?retryWrites=true&w=majority`).then(() => { app.listen(PORT, () => console.log("server start at " + PORT)) }).catch(() => console.log("error"));
