2329d0c0e3225ec57d97e0a4d8306fb9ee57d5e3
Mohd Sharique <76559067+shariquefaridi7@users.noreply.github.com>
2024-02-24 02:30:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,5 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = 4000;
 app.use("/", empInfoRoute);
 app.use("/", sheetRoute);
+app.get("/",(req,res)=>{
+  res.json("hello");
+})
 
 mongoose.connect(`mongodb+srv://project1:jtQgREqnQpXqKB9a@cluster0.vjk5qmt.mongodb.net/?retryWrites=true&w=majority`).then(() => { app.listen(PORT, () => console.log("server start at " + PORT)) }).catch(() => console.log("error"));
